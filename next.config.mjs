cebbb22774561293aebec3297607353fc89e2563
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-12-21 15:15:14 -0800

    add rewrites in config.mjs to allow clerk send requests to the server

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,18 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Be cautious with this in production
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+          {
             key: 'Set-Cookie',
             value: '__vercel_live_token=value; Path=/; Secure; SameSite=None',
           },
@@ -13,6 +25,15 @@ const nextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/webhooks/clerk/:path*',
+        destination: '/api/webhooks/clerk/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
