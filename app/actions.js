108361a40dccacb5f232503eae4413f4f741f31e
Daniel Kuznetsov <danilgkuznet@icloud.com>
2024-12-20 22:01:09 -0800

    Implement test case for supabase data storage

--- app/actions.js
+++ app/actions.js
@@ -1,6 +1,7 @@
 'use server';
 
 import supabase from '@/utils/supabase/client';
+import { revalidatePath } from 'next/cache'
 
 export async function createAction() {
     console.log('Creating action...');
@@ -16,4 +17,28 @@ export async function createAction() {
 
 export async function getAction() {
     console.log('Getting action...');
+}
+
+export async function createTodo() {
+    const { data, error } = await supabase.from("todos").insert({
+        todo: "Test Todo"
+    });
+
+    if (error) {
+        console.error(error);
+        return { success: false, message: error.message };
+    }
+
+    revalidatePath('/');
+}
+
+export async function removeTodo() {
+    const { data, error } = await supabase.from("todos").delete().eq("id", 5);
+
+    if (error) {
+        console.error(error);
+        return { success: false, message: error.message };
+    }
+
+    revalidatePath('/');
 }
--- components/TestPage.jsx
+++ components/TestPage.jsx
@@ -1,10 +1,10 @@
 'use server'
 
-import { createAction, getAction } from "../app/actions.js";
+import { createAction, getAction, createTodo, removeTodo } from "../app/actions.js";
 
 export default async function TestPage() {
     console.log('TestPage');
-    const { data, error } = await createAction();
+    const { data } = await createAction();
     await getAction();
 
     console.log(data);
@@ -13,6 +13,9 @@ export default async function TestPage() {
         <div>
             <h1>TestPage</h1>
             <div>{data.map((item, index) => <p key={index}>{item.id}</p>)}</div>
+
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-md" onClick={createTodo}>Create Todo</button>
+            <button className="bg-red-500 text-white px-4 py-2 rounded-md" onClick={removeTodo}>Remove Todo</button>
         </div>
     )
 }
