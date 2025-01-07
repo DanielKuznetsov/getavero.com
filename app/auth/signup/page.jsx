f7e7bc0fb85c41a24008cdaeaff0652f7e81eab8
Daniel Kuznetsov <danilgkuznet@icloud.com>
2025-01-07 10:40:23 -0800

    there will be no signup page as admin is the only person to create orders

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -139,142 +139,141 @@ export default function SignUpPage() {
     }
   }
 
-  return (
-    <div className="min-h-screen flex flex-col">
-      <div className="flex-1 flex flex-col justify-center items-center p-4">
-        <div className="w-full">
-          <div className="mb-8 absolute top-4 left-4">
-            <Button asChild variant="outline">
-              <Link href="/" className="flex items-center gap-2">
-                <ArrowLeft size={14} />
-                <span>Back to Homepage</span>
-              </Link>
-            </Button>
-          </div>
+  return null
+  // <div className="min-h-screen flex flex-col">
+  //   <div className="flex-1 flex flex-col justify-center items-center p-4">
+  //     <div className="w-full">
+  //       <div className="mb-8 absolute top-4 left-4">
+  //         <Button asChild variant="outline">
+  //           <Link href="/" className="flex items-center gap-2">
+  //             <ArrowLeft size={14} />
+  //             <span>Back to Homepage</span>
+  //           </Link>
+  //         </Button>
+  //       </div>
 
-          <motion.div
-            className="bg-white sm:p-8 p-6 rounded-lg shadow-authCard w-full max-w-md mx-auto"
-            initial={{ scale: 0.85, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{ duration: 0.3 }}
-          >
-            <div className="text-center mb-6">
-              <h1 className="text-2xl font-semibold mb-2">
-                {verifying ? "Verify Your Email" : "Create an Account"}
-              </h1>
-              <p className="text-gray-600 text-sm">
-                {verifying
-                  ? `We've sent a verification code to ${email}.`
-                  : "Our users save up to 3 hours a week during recruitment times with Millo."}
-              </p>
-            </div>
+  //       <motion.div
+  //         className="bg-white sm:p-8 p-6 rounded-lg shadow-authCard w-full max-w-md mx-auto"
+  //         initial={{ scale: 0.85, opacity: 0 }}
+  //         animate={{ scale: 1, opacity: 1 }}
+  //         transition={{ duration: 0.3 }}
+  //       >
+  //         <div className="text-center mb-6">
+  //           <h1 className="text-2xl font-semibold mb-2">
+  //             {verifying ? "Verify Your Email" : "Create an Account"}
+  //           </h1>
+  //           <p className="text-gray-600 text-sm">
+  //             {verifying
+  //               ? `We've sent a verification code to ${email}.`
+  //               : "Our users save up to 3 hours a week during recruitment times with Millo."}
+  //           </p>
+  //         </div>
 
-            <FormProvider {...form}>
-              {verifying ? (
-                <div className='space-y-4'>
-                  <form onSubmit={handleVerification}>
-                    <FormItem className="flex items-center justify-center mb-2">
-                      <FormControl>
-                        <InputOTP
-                          maxLength={6}
-                          value={code}
-                          onChange={(value) => setCode(value)}
-                        >
-                          <InputOTPGroup>
-                            <InputOTPSlot index={0} />
-                            <InputOTPSlot index={1} />
-                            <InputOTPSlot index={2} />
-                            <InputOTPSlot index={3} />
-                            <InputOTPSlot index={4} />
-                            <InputOTPSlot index={5} />
-                          </InputOTPGroup>
-                        </InputOTP>
-                      </FormControl>
-                    </FormItem>
+  //         <FormProvider {...form}>
+  //           {verifying ? (
+  //             <div className='space-y-4'>
+  //               <form onSubmit={handleVerification}>
+  //                 <FormItem className="flex items-center justify-center mb-2">
+  //                   <FormControl>
+  //                     <InputOTP
+  //                       maxLength={6}
+  //                       value={code}
+  //                       onChange={(value) => setCode(value)}
+  //                     >
+  //                       <InputOTPGroup>
+  //                         <InputOTPSlot index={0} />
+  //                         <InputOTPSlot index={1} />
+  //                         <InputOTPSlot index={2} />
+  //                         <InputOTPSlot index={3} />
+  //                         <InputOTPSlot index={4} />
+  //                         <InputOTPSlot index={5} />
+  //                       </InputOTPGroup>
+  //                     </InputOTP>
+  //                   </FormControl>
+  //                 </FormItem>
 
-                    <div className="text-center text-xs text-neutral-500 font-light mb-4">
-                      {code === "" ? (
-                        <>Enter your one-time password.</>
-                      ) : (
-                        <>You entered: {code}</>
-                      )}
-                    </div>
+  //                 <div className="text-center text-xs text-neutral-500 font-light mb-4">
+  //                   {code === "" ? (
+  //                     <>Enter your one-time password.</>
+  //                   ) : (
+  //                     <>You entered: {code}</>
+  //                   )}
+  //                 </div>
 
-                    <LoadingButton isLoading={isLoading} icon={arrowRightIcon} loadingText="Verifying..." buttonText="Verify" className="w-full" />
-                  </form>
-                  <Button
-                    variant="link"
-                    className="w-full text-sm"
-                    onClick={handleResendCode}
-                  >
-                    Resend verification code
-                  </Button>
-                </div>
-              ) : (
-                <Form {...form}>
-                  <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
-                    <div className="flex gap-6">
-                      <FormField
-                        control={form.control}
-                        name="firstName"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel className='text-primary'>First Name</FormLabel>
-                            <FormControl>
-                              <Input placeholder="Enter your first name" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
+  //                 <LoadingButton isLoading={isLoading} icon={arrowRightIcon} loadingText="Verifying..." buttonText="Verify" className="w-full" />
+  //               </form>
+  //               <Button
+  //                 variant="link"
+  //                 className="w-full text-sm"
+  //                 onClick={handleResendCode}
+  //               >
+  //                 Resend verification code
+  //               </Button>
+  //             </div>
+  //           ) : (
+  //             <Form {...form}>
+  //               <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
+  //                 <div className="flex gap-6">
+  //                   <FormField
+  //                     control={form.control}
+  //                     name="firstName"
+  //                     render={({ field }) => (
+  //                       <FormItem>
+  //                         <FormLabel className='text-primary'>First Name</FormLabel>
+  //                         <FormControl>
+  //                           <Input placeholder="Enter your first name" {...field} />
+  //                         </FormControl>
+  //                         <FormMessage />
+  //                       </FormItem>
+  //                     )}
+  //                   />
 
 
-                      <FormField
-                        control={form.control}
-                        name="lastName"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormLabel className='text-primary'>Last Name</FormLabel>
-                            <FormControl>
-                              <Input placeholder="Enter your last name" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                    </div>
+  //                   <FormField
+  //                     control={form.control}
+  //                     name="lastName"
+  //                     render={({ field }) => (
+  //                       <FormItem>
+  //                         <FormLabel className='text-primary'>Last Name</FormLabel>
+  //                         <FormControl>
+  //                           <Input placeholder="Enter your last name" {...field} />
+  //                         </FormControl>
+  //                         <FormMessage />
+  //                       </FormItem>
+  //                     )}
+  //                   />
+  //                 </div>
 
-                    <FormField
-                      control={form.control}
-                      name="email"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel className='text-primary'>Email</FormLabel>
-                          <FormControl>
-                            <Input type="email" placeholder="Enter your email" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
+  //                 <FormField
+  //                   control={form.control}
+  //                   name="email"
+  //                   render={({ field }) => (
+  //                     <FormItem>
+  //                       <FormLabel className='text-primary'>Email</FormLabel>
+  //                       <FormControl>
+  //                         <Input type="email" placeholder="Enter your email" {...field} />
+  //                       </FormControl>
+  //                       <FormMessage />
+  //                     </FormItem>
+  //                   )}
+  //                 />
 
-                    {/* CAPTCHA Widget */}
-                    <div id="clerk-captcha"></div>
+  //                 {/* CAPTCHA Widget */}
+  //                 <div id="clerk-captcha"></div>
 
-                    <LoadingButton isLoading={isLoading} icon={arrowRightIcon} loadingText="Signing up..." buttonText="Try for Free" className="w-full" />
-                  </form>
-                </Form>
-              )}
-            </FormProvider>
+  //                 <LoadingButton isLoading={isLoading} icon={arrowRightIcon} loadingText="Signing up..." buttonText="Try for Free" className="w-full" />
+  //               </form>
+  //             </Form>
+  //           )}
+  //         </FormProvider>
 
-            {!verifying && (
-              <div className='flex flex-col gap-2 text-[14px] font-light text-neutral-500 items-center mt-6'>
-                <Link href="/auth/login">Already have an account? Sign In</Link>
-              </div>
-            )}
-          </motion.div>
-        </div>
-      </div>
-    </div>
-  )
+  //         {!verifying && (
+  //           <div className='flex flex-col gap-2 text-[14px] font-light text-neutral-500 items-center mt-6'>
+  //             <Link href="/auth/login">Already have an account? Sign In</Link>
+  //           </div>
+  //         )}
+  //       </motion.div>
+  //     </div>
+  //   </div>
+  // </div>
 }
