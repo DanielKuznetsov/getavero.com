1cc315ab889155c832826c21b680ddd265f86b47
Daniel Kuznetsov <danilgkuznet@icloud.com>
2025-01-07 11:51:47 -0800

    added a 'new restaurant form'

---
+++ app/actions/restaurant-actions.js
@@ -0,0 +1,33 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import supabase from '@/utils/supabase/client';
+
+export async function addNewRestaurant(restaurantData) {
+    // console.log("Adding new restaurant...");
+    // console.log(restaurantData);
+
+    const { data, error } = await supabase.from("restaurants").select("*").eq("business_email_address", restaurantData.email_address);
+
+    // console.log("data one");
+    // console.log(data);
+    // console.log("error one");
+    // console.log(error);
+
+    if (data.length > 0) {
+        return { success: false, message: "Restaurant already exists" };
+    }
+
+    const { data: restaurant_data, error: restaurant_error } = await supabase.from("restaurants").insert({
+        business_name: restaurantData.businessName,
+        business_address: restaurantData.businessAddress,
+        business_phone_number: restaurantData.phoneNumber,
+        business_email_address: restaurantData.email_address,
+    });
+
+    if (restaurant_error) {
+        return { success: false, message: "Error adding restaurant" };
+    } else {
+        return { success: true, message: "Restaurant added successfully", restaurant_data: restaurant_data };
+    }
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
+import NewRestaurantForm from "@/components/ui/custom-large/new-restaurant-form";
 
 export default async function Home() {
 
   return (
-    <h1>Hello World</h1>
+    <div className="flex gap-4 p-4">
+      {/* Add New Restaurant */}
+      <NewRestaurantForm />
+
+      {/* Edit Restaurant */}
+
+      {/* Add Menu Items */}
+
+      {/* Edit Menu Items */}
+    </div>
   );
 }
+
+// Need to add several components here:
+// 1. New restaurant form
+// 2. Menu items form
+
